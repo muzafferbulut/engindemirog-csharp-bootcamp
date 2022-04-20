612229cf2a4e98835bc4116d05162cc5164103ce
Muzaffer Bulut <muzafferb042@gmail.com>
2022-04-20 13:25:29 +0300

    Üçüncü günün ödevi (generic metot yazılması) tamamlandı.

---
+++ KampIntro/HW_GenericDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_GenericDictionary
+{
+    class MyDictionary<Tkey, Tvalue> // burada anahtar değer çiftlerinin tipleri temsil edilir.
+    {
+        Tkey[] keys; // burada key value çiftleri array olarak tanımlanır. Bu çiftler her yerden erişilebilir durumdadır.
+        Tvalue[] values;
+        public MyDictionary()
+        {
+            /* class newlendiği anda çalışacak blok */
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            // bir temp arrayde referans numarası tutulur. Referans numarasını önceki değerleri kaybetmemek için kullanacağız.
+            Tkey[] tempArrayKey = keys;
+            Tvalue[] tempArrayValue = values;
+
+            // Ekleme işlemi olduğu için dizi newlenir ve eleman sayısı bir artırılır.
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+
+            // bu bölümde dictionary yapısında önceden var olan değerler eklenir
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+                values[i] = tempArrayValue[i];
+            }
+
+            // parametre olarak verilen değerler eklenir
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+    }
+}
---
+++ KampIntro/HW_GenericDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW_GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(1, "Muzaffer");
+            students.Add(1, "Bulut");
+
+            Console.WriteLine(students.Count);
+        }
+    }
+}
