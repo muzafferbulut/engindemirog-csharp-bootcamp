0be4a9eb4aaedce6482373668447d97e10edd5a0
Muzaffer Bulut <muzafferb042@gmail.com>
2022-04-22 02:24:43 +0300

    Abstract ve interface class yapıları derinlemesine incelendi. OOP teknikleri uygulandı

---
+++ KampIntro/InterfaceAbstractDemo/Abstract/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Abstract
+{
+    public abstract class CustomerManager : ICustomerService
+    {
+        // base görevi görecek
+        public void Save(Customer customer)
+        {
+            Console.WriteLine("Veri tabanına kayıt işlemi başarıyla gerçekleşti.");
+        }
+
+        internal void Save(object p, object )
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KampIntro/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new StarbucksCustomerManager();
+
+        }
+    }
+}
