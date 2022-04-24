a170cfcc5819e1c7b8c52e4f5ddd4a1d38ad87ee
Muzaffer Bulut <muzafferb042@gmail.com>
2022-04-24 17:13:18 +0300

    Abstract ve interface sınıflar tekrar edildi.

---
+++ InterfaceAbstractDemo/InterfaceAbstractDemo/Entities/Customer.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Entities
+{
+    public class Customer:IEntity
+    {
+        // müşteriye ait nitelikler
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TcNo { get; set; }
+    }
+}
---
+++ InterfaceAbstractDemo/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Concrete;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Kahve dükkanları için müşteri yönetimi yapan bir sistem yazmak istiyoruz. Strabucks ve Nero
+            firması için çalışıyoruz. İki firma da müşterilerini veri tabanına kaydetmek istiyor. Starbucks
+            müşterileri kaydederken mutlaka mernis doğrulaması istiyor ancak nero böyle bir şey istemiyor.
+            Starbucks müşteriler için her kahve alımında yıldız kazandırmak istiyor.
+             */
+
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1998,10,20), FirstName="Muzaffer",
+                                                LastName="Bulut", Id=1, TcNo="4546546"});
+
+            
+        }
+    }
+}
