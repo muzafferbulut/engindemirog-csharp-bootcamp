8f8f9bfcf28a7c9b45f3fecebce8c515596f1c57
Muzaffer Bulut <muzafferb042@gmail.com>
2022-04-27 18:59:07 +0300

    7. hafta linq ve katmanlı mimariler eğitimi tamamlandı.

---
+++ MyFinalProject/ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
---
+++ MyFinalProject/Data Access/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products; // global değişken
+        public InMemoryProductDal()
+        {
+            // veri tabanından geliyor gibi simule edelim
+            _products = new List<Product>
+            {
+                new Product{ProductId=1, CategoryId=1, ProductName="Bardak", UnitPrice=15, UnitInStock=15},
+                new Product{ProductId=2, CategoryId=1, ProductName="Kamera", UnitPrice=500, UnitInStock=3},
+                new Product{ProductId=3, CategoryId=2, ProductName="Telefon", UnitPrice=1500, UnitInStock=2},
+                new Product{ProductId=4, CategoryId=2, ProductName="Klavye", UnitPrice=150, UnitInStock=65},
+                new Product{ProductId=5, CategoryId=2, ProductName="Fare", UnitPrice=85, UnitInStock=1}
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            // _products.Remove(product); bu şekilde silme işlemi gerçekleşmez. Çünkü referans tipler referans
+            // numarasından gider. Nesne bilgilerinin aynı olması önemli değil. Referans numarasını arar bulursa siler.
+
+            // linq bilmeseydik
+            Product productToDelete = null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+            // linq ile
+            productToDelete = _products.SingleOrDefault(p =>p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int CategoryId)
+        {
+            // şarta uyan tüm elemanlar yeni bir liste olarak geri döndürür
+            return _products.Where(p => p.CategoryId == CategoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = null;
+            productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId); // güncellenecek refereansı bulacağız
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.ProductId = product.ProductId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.CategoryId = product.CategoryId;
+        }
+    }
+}
